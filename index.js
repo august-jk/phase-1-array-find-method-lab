b3ce27f5c3f89276ced1c8b261f32e180ccafddd
Aletha Wheeless <augustkama98@gmail.com>
2022-05-09 20:13:16 -1000

    commit tree 56a00cb20bb1f38cba42544d94f7ab93ff98d9e7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+]
+
+
+
+function superbowlWin(record) {
+    let newArr = {}
+    newArr = record.find(function(record){
+        return record.result === 'W'
+    }, undefined)
+   return newArr.year
+}
+  superbowlWin(record)
